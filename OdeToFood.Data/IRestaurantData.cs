f10e4b91bd8a4b2d0776078c6027600b6717e006
menaka.siri <menaka.sirisena@eyepax.com>
2021-02-27 22:35:45 +0530

    added restaurant detail page with basic navigation

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -22,8 +22,8 @@ namespace OdeToFood.Data
             restaurants = new List<Restaurant>()
             {
                 new Restaurant{Id=1, Name = "Scott's Pizza", Location="Maryland", Cuisine=CuisineType.Italian},
-                new Restaurant{Id=1, Name = "Cinnamon Club", Location="London", Cuisine=CuisineType.Indian},
-                new Restaurant{Id=1, Name = "La Costa", Location="California", Cuisine=CuisineType.Mexican}
+                new Restaurant{Id=2, Name = "Cinnamon Club", Location="London", Cuisine=CuisineType.Indian},
+                new Restaurant{Id=3, Name = "La Costa", Location="California", Cuisine=CuisineType.Mexican}
             };
         }
         public IEnumerable<Restaurant> GetAll()
